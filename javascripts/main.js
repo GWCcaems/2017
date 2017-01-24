85ad94dfd1a671e79f9e90871c3d395f9cf61a0a
GWCcaems <kathryn.bradley@bcsemail.org>
2017-01-24 08:32:49 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+var subject;
+var grade;
+
+function chooseArea () {
+	//user clicks grade
+	grade = user.Input;
+	return grade;
+	//user clicks subject
+	subject = user.Input;
+	return subject;
+}
+
+function sendUser (grade, subject) {
+
+
+}
