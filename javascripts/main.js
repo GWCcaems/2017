7bdb658e13c9943d963c83b0079dfc13beb86b92
GWCcaems <kathryn.bradley@bcsemail.org>
2017-05-05 08:30:15 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,22 @@
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click', function() {
+    //$(this).next().toggle();
+    $(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+  });
+}
+
+$(document).ready(main);
+
+
+
+
+
+
 var subject;
 var grade;
 
